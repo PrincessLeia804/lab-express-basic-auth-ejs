a47760eadf7dc6bf867bfa866a324b43ec4b063c
Lea Model <lea.skywalker309@gmail.com>
2023-07-11 16:40:53 +0200

    redirect to user profile

--- routes/auth.js
+++ routes/auth.js
@@ -2,26 +2,35 @@ const router = require('express').Router()
 const bcrypt = require('bcryptjs');
 const User = require('../models/User.model');
 
+
 // GET sign-up page
 router.get("/signup", (req, res) => {
     res.render("auth/signup");
 })
 
+// GET profile-page path
+router.get("/user-profile", (req, res) => {
+    res.render("auth/user-profile")
+})
+
+
 // POST sign-up page
 router.post("/signup", async (req, res) => {
  
     const payload = { ...req.body}
+    console.log('payload: ', payload);
 
     // PW entered not into the DB
     delete payload.password
 
     // encrypt PW with 13 rounds
     const salt = bcrypt.genSaltSync(13)
-    payload.passwordHash = bcrypt.hashSync(req.body.password, salt)
+    payload.passwordHash = await bcrypt.hashSync(req.body.password, salt)
 
     try {
         const newUser = await User.create(payload)
-        res.send(newUser)
+        res.redirect("/auth/user-profile")
+        // res.send(newUser)
     } catch (error) {
         console.log("The new user couldn't be created");
     }
